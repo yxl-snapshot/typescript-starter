540d2a1a42992e9bc14e1d5f2419674f8f80138b
yxliang01 <yxliang01@users.noreply.github.com>
2018-12-17 02:14:39 +0000

    Remove dependency "md5-file" and CI integration test

--- src/types/cli.d.ts
+++ src/types/cli.d.ts
@@ -2,5 +2,4 @@
 // works out of the box for downstream users. This file is deleted by the CLI,
 // so its purpose is just to squelch noImplicitAny errors.
 declare module 'github-username';
-declare module 'md5-file';
 declare module 'validate-npm-package-name';
